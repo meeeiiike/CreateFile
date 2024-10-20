f2b31f515e24b3b04560d05d34a0e85f701145b9
meeeiiike <g00407095@atu.ie>
2024-10-20 01:56:44 +0100

    Created file and object outputFile

--- src/main/java/ie/atu/idk/CreateFile.java
+++ src/main/java/ie/atu/idk/CreateFile.java
@@ -1,7 +1,19 @@
 package ie.atu.idk;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class CreateFile {
 
+    public static void main(String[] args)throws IOException {
 
+        File myFile = new File("MyFile1.txt");
+        System.out.println("My file is located at " + myFile.getAbsolutePath());
+
+        FileWriter outputFile = new FileWriter("MyFile1.txt", true);
+
+    }
 
 }
